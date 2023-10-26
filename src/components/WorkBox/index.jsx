879d2db5ec1978b124b9d70cdf549ba760787c07
Mertcan Kose <mertcankose97@outlook.com>
2023-10-26 22:45:22 +0300

    fix work boxes and add new cv

--- src/components/WorkBox/index.jsx
+++ src/components/WorkBox/index.jsx
@@ -9,7 +9,7 @@ const WorkBox = ({ item, extra, className, imageClassName, ...props }) => {
   return (
     <Link to={item?.generalLink} href={item.generalLink} className={[styles.workBox, className, "relative"].join(" ")} {...props}>
       <div className="flex flex-col h-full">
-        <div>
+        <div className="flex flex-col items-start justify-start">
           <img className={[styles.image, imageClassName].join(" ")} src={item?.image} alt="Project" />
           <h3 className={styles.title}>{item?.title}</h3>
           <p className={styles.description}>{item?.description}</p>
